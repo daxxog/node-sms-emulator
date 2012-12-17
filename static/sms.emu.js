f67a87d9874757a927d952d55695d5743540c439
David Volm <david@volminator.com>
2012-12-17 15:17:13 -0600

    PIMPED OUT THE CLIENT (sorta)

--- static/sms.emu.js
+++ static/sms.emu.js
@@ -12,24 +12,35 @@ Math.gendigits = function(dig) {
 };
 
 $(function() {
-    $('#smsout').focus();
+    var mod_number = function() {
+        $('#smsout').focus();
+        
+        if($('#vnumber').html() == '') {
+            $('#vnumber').html(Math.gendigits(10));
+        }
+        
+        socket.on('sms.emu.to.'+$("#vnumber").html(), function (data) {
+            $('<div></div>').html('<b>sms.emu: </b><pre>'+data+'</pre>').appendTo('#smsin');
+        });
+    }; mod_number();
     
-    if($('#vnumber').html() == '') {
-        $('#vnumber').html(Math.gendigits(10));
-    }
-    
-    socket.on('sms.emu.to.'+$("#vnumber").html(), function (data) {
-        $('<div></div>').html('<b>sms.emu: </b>'+data).appendTo('#smsin');
-    });
-    
-    $("#send").click(function() {
-        $('<div></div>').html('<i>me: </i>'+$("#smsout").val()).appendTo('#smsin');
+    $('#send').click(function() {
+        $('<div></div>').html('<i>me: </i><pre>'+$('#smsout').val()+'</pre>').appendTo('#smsin');
         socket.emit('sms.emu.to.server', {
             number: $("#vnumber").html(),
             msg: $("#smsout").val()
         });
         
-        $("#smsout").val('');
+        $('#smsout').val('');
         $('#smsout').focus();
+    });
+    
+    $('#clear').click(function() {
+        $('#smsin').html('');
+    });
+    
+    $('#vnumber').click(function() {
+        $('#vnumber').html(prompt('New #?', Math.gendigits(10)));
+        mod_number();
     });
 });
